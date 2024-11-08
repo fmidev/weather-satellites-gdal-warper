26af151e093ac29b1453fcd6c601cc31363c6bc6
Panu Lahtinen <panu.lahtinen@fmi.fi>
2024-11-08 14:24:35 +0200

    Add SIGTERM handling to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/bash
 
+_term() {
+  echo "Entrypoint caught SIGTERM signal"
+  kill -TERM "$child" 2>/dev/null
+  echo "Waiting for child process to exit"
+  wait "$child"
+}
+
+trap _term SIGTERM
+
 source /opt/conda/.bashrc
 source /config/env-variables
 
 micromamba activate
 
-/opt/conda/bin/gdal_warper.py /config/gdal_warper.yaml
+/opt/conda/bin/gdal_warper.py /config/gdal_warper.yaml &
+
+child=$!
+
+wait "$child"
